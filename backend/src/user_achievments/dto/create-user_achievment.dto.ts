172fe1fbd130202c8bb57632ecf5ba4076e05b4b
Simon <simon.aeby@laendrun.ch>
2023-09-28 15:56:44 +0200

    added backend api documentation

--- backend/src/user_achievments/dto/create-user_achievment.dto.ts
+++ backend/src/user_achievments/dto/create-user_achievment.dto.ts
@@ -1,12 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt } from 'class-validator';
 
+
 export class CreateUserAchievmentDto {
+	@ApiProperty({example: '1', description: 'Current progress of the achievment.'})
   @IsInt({ message: '$property must be an integer value.' })
   current: number;
 
+	@ApiProperty({example: '1', description: 'User id.'})
   @IsInt({ message: '$property must be an integer value.' })
   user_id: number;
 
+  @ApiProperty({example: '1', description: 'Achievment id.'})
   @IsInt({ message: '$property must be an integer value.' })
   achievment_id: number;
 }
--- backend/src/user_achievments/user_achievments.controller.ts
+++ backend/src/user_achievments/user_achievments.controller.ts
@@ -12,7 +12,10 @@ import { AuthGuard } from 'src/auth.guard';
 import { UserAchievmentsService } from './user_achievments.service';
 import { CreateUserAchievmentDto } from './dto/create-user_achievment.dto';
 import { UpdateUserAchievmentDto } from './dto/update-user_achievment.dto';
+import { UserAchievmentResponse } from './dto/userachievment.response'
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
+@ApiTags('User Achievments')
 @Controller('userachievments')
 export class UserAchievmentsController {
   constructor(
@@ -20,6 +23,10 @@ export class UserAchievmentsController {
   ) {}
 
   @Post()
+  @ApiBearerAuth()
+  @ApiOperation({summary: 'Create a user_achievments.'})
+  @ApiUnauthorizedResponse({description: 'Unauthorized.'})
+  @ApiResponse({status: 201, description: 'User_achievment created.',  type: CreateUserAchievmentDto})
   @UseGuards(AuthGuard)
   create(@Body() createUserAchievmentDto: CreateUserAchievmentDto) {
     return this.userAchievmentsService.create(createUserAchievmentDto);
@@ -27,24 +34,40 @@ export class UserAchievmentsController {
 
   @Get()
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({summary: 'Get all user_achievments.'})
+  @ApiUnauthorizedResponse({description: 'Unauthorized.'})
+  @ApiResponse({status: 200, description: 'Array of all user_achievments.', type: [UserAchievmentResponse]})
   findAll() {
     return this.userAchievmentsService.findAll();
   }
 
   @Get(':id')
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({summary: 'Get user_achievment by id.'})
+  @ApiUnauthorizedResponse({description: 'Unauthorized.'})
+  @ApiResponse({status: 200, description: 'User_achievment by id.', type: UserAchievmentResponse})
   findOne(@Param('id') id: string) {
     return this.userAchievmentsService.findOne(+id);
   }
 
   @Get('users/:id')
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({summary: 'Get user_achievment by user id.'})
+  @ApiUnauthorizedResponse({description: 'Unauthorized.'})
+  @ApiResponse({status: 200, description: 'User_achievment by user id.', type: [UserAchievmentResponse]})
   findUserAchievments(@Param('id') id: number) {
 	return this.userAchievmentsService.findUserAchievments(id);
   }
 
   @Patch(':id')
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({summary: 'Update user_achievment by id.'})
+  @ApiUnauthorizedResponse({description: 'Unauthorized.'})
+  @ApiResponse({status: 200, description: 'User_achievment updated.'})
   update(
     @Param('id') id: string,
     @Body() updateUserAchievmentDto: UpdateUserAchievmentDto,
@@ -54,6 +77,10 @@ export class UserAchievmentsController {
 
   @Delete(':id')
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({summary: 'Delete user_achievment by id.'})
+  @ApiUnauthorizedResponse({description: 'Unauthorized.'})
+  @ApiResponse({status: 200, description: 'User_achievment deleted.'})
   remove(@Param('id') id: string) {
     return this.userAchievmentsService.remove(+id);
   }
