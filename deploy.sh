3d72a794ac3f5f1d1139a773f55cf569a929ead9
Simon <simon.aeby@laendrun.ch>
2023-12-12 21:34:17 +0100

    changed

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+	echo "usage: ./deploy.sh <branch>"
+	exit 1
+fi
+
+BRANCH=$1
+REPO="git@github.com:JeanCarlen/TrD.git"
+
+# Create a temporary directory to store the files
+TMP_DIR="/tmp/deploy-$(date +%s)"
+mkdir -p $TMP_DIR
+cd $TMP_DIR
+
+# Start by cloning the branch from Github
+git clone -b $BRANCH $REPO --depth 1 TrD
+
+# Remove all docker thingys
+docker ps -aq | xargs docker stop | xargs docker rm
+docker images -aq | xargs docker image rm
+docker volume ls -q | xargs docker volume rm
+
+cd TrD/
+cp /env/back.env backend/.env
+find ./ -type f -exec sed -i -e 's|http://localhost:8080|https://trd.laendrun.ch|g' {} \;
+find ./ -type f -exec sed -i -e 's|http://localhost:3001|https://trd.laendrun.ch:3001|g' {} \;
+docker compose up -d --build
