a2ec1cef2c5daf1ab98dfd9e4d6457efaad3f938
Jean Carlen <jm.carlen1994@gmail.com>
2023-12-13 14:25:12 +0100

    css done

--- frontend/src/Components/force_logout.tsx
+++ frontend/src/Components/force_logout.tsx
@@ -24,7 +24,7 @@ const ForceLogout: React.FC<{}> = () => {
 
   useEffect(() => {
     gsocket.on("force-logout", () => {
-      toast.error("You have been got logged somewhere else");
+      toast.error("You got logged somewhere else");
 
       Cookies.remove("token");
       gsocket.disconnect();
