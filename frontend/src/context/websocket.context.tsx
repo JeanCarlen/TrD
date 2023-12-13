63ae4c6eed0cecebb3a7b8949301ba6228f84cc1
Simon Aeby <saeby@student.42lausanne.ch>
2023-12-13 12:27:56 +0100

    mazbe sometime it'll work

--- frontend/src/context/websocket.context.tsx
+++ frontend/src/context/websocket.context.tsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { io } from 'socket.io-client'
 
-export const gsocket = io('http://localhost:3001', { path: '/api', autoConnect: false });
+export const gsocket = io('wss://trd.laendrun.ch/', { path: '/api', autoConnect: false });
 
 export const WebsocketContext = createContext(gsocket);
 
