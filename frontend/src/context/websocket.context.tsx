67fb65b5b982aa779b4b594a04e8fbdd154bdc1a
Simon Aeby <saeby@student.42lausanne.ch>
2023-12-13 16:00:47 +0100

    it is working, now i need kofee :notebook:

--- frontend/src/context/websocket.context.tsx
+++ frontend/src/context/websocket.context.tsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { io } from 'socket.io-client'
 
-export const gsocket = io('http://10.12.2.5:8080', { path: '/api' });
+export const gsocket = io('http://10.12.2.5:3001', { path: '/api' });
 // export const gsocket = io.connect()
 
 export const WebsocketContext = createContext(gsocket);
